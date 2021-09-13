fcae97168bcd7267f1d17de06ee5f69bba91a47e
Odilomar <odilomarjunior.contact@gmail.com>
2021-09-13 17:49:04 -0300

    feat: update model with entity decorator

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -1,13 +1,38 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
 export class User {
+  @PrimaryGeneratedColumn("uuid")
   id?: string;
+
+  @Column()
   name!: string;
+
+  @Column()
   email!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
 
   private constructor({ name, email }: User) {
     return Object.assign(this, { name, email });
   }
 
-  static create({ name, email }: User) {
-    return new User({ name, email });
+  static create({
+    name,
+    email,
+    createdAt = new Date(),
+    updatedAt = new Date(),
+  }: User) {
+    return new User({ name, email, createdAt, updatedAt });
   }
 }
